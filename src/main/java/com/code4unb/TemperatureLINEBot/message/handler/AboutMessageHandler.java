1538d7f7e5a9bfd8f53ad349ae55c2e25bd1c739
tkhs-dev <tokuhausu@gmail.com>
2021-06-05 23:57:04 +0900

    refactor:change TimeZone to system-default(Asia/Tokyo)

--- src/main/java/com/code4unb/TemperatureLINEBot/message/handler/AboutMessageHandler.java
+++ src/main/java/com/code4unb/TemperatureLINEBot/message/handler/AboutMessageHandler.java
@@ -9,6 +9,9 @@ import com.linecorp.bot.model.message.FlexMessage;
 import com.linecorp.bot.model.message.Message;
 import org.springframework.boot.info.BuildProperties;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 @Handler
 public class AboutMessageHandler extends MessageHandler {
     public AboutMessageHandler(){
@@ -18,7 +21,7 @@ public class AboutMessageHandler extends MessageHandler {
     public Message HandleMessage(ReceivedMessage message) {
         BuildProperties prop = TemperatureLineBotApplication.BuildProperties;
         String json = FlexJson.LoadMessageJson("about");
-        json = String.format(json, prop.getName(), prop.getVersion(), prop.getTime().toString());
+        json = String.format(json, prop.getName(), prop.getVersion(), prop.getTime().atZone(ZoneId.systemDefault()).toString());
         return new FlexMessage("version:"+prop.getVersion(),FlexJson.CreateFlexContainer(json));
     }
 }
