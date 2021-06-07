5a4b8787348da9384fffb77fbed74a5a7510e8f8
tkhs-dev <tokuhausu@gmail.com>
2021-06-07 22:41:09 +0900

    refactor: use @Autowired BuildProperties instead of original creating instance process

--- src/main/java/com/code4unb/TemperatureLINEBot/message/handler/AboutMessageHandler.java
+++ src/main/java/com/code4unb/TemperatureLINEBot/message/handler/AboutMessageHandler.java
@@ -6,6 +6,7 @@ import com.code4unb.TemperatureLINEBot.message.ReceivedMessage;
 import com.code4unb.TemperatureLINEBot.util.FlexJson;
 import com.linecorp.bot.model.message.FlexMessage;
 import com.linecorp.bot.model.message.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Component;
 
@@ -13,14 +14,16 @@ import java.time.ZoneId;
 
 @Component
 public class AboutMessageHandler extends MessageHandler {
+    @Autowired
+    BuildProperties buildProperties;
+
     public AboutMessageHandler(){
         super("About","アプリについて","version","バージョン");
     }
     @Override
     public Message HandleMessage(ReceivedMessage message) {
-        BuildProperties prop = TemperatureLineBotApplication.BuildProperties;
         String json = FlexJson.LoadMessageJson("about");
-        json = String.format(json, prop.getName(), prop.getVersion(), prop.getTime().atZone(ZoneId.systemDefault()).toString());
-        return new FlexMessage("version:"+prop.getVersion(),FlexJson.CreateFlexContainer(json));
+        json = String.format(json, buildProperties.getName(), buildProperties.getVersion(), buildProperties.getTime().atZone(ZoneId.systemDefault()).toString());
+        return new FlexMessage("version:"+buildProperties.getVersion(),FlexJson.CreateFlexContainer(json));
     }
 }
