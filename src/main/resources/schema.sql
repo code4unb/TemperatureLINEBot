f237ab9b8dcbc0dd9b582c63cd6a6e1af28eef10
tkhs <38522336+tkhs-dev@users.noreply.github.com>
2021-06-09 00:09:40 +0900

    feature: add schema.sql file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,12 @@
-create table member
+create table user_data_entity
 (
     id varchar not null
         constraint member_pk
             primary key auto_increment,
-    line_id varchar not null
-    ,grade varchar not null
-    ,class integer not null
-    ,number integer not null
+    line_id varchar
+    ,grade varchar
+    ,class integer
+    ,number integer
     ,first_name varchar
     ,last_name varchar
 );
