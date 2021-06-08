88779817067d74e5f96878e54327d6a3e4dc5de7
tkhs <38522336+tkhs-dev@users.noreply.github.com>
2021-06-09 07:30:07 +0900

    fix: fix sql column name to upper

--- src/main/java/com/code4unb/TemperatureLINEBot/db/UserDataEntity.java
+++ src/main/java/com/code4unb/TemperatureLINEBot/db/UserDataEntity.java
@@ -3,36 +3,42 @@ package com.code4unb.TemperatureLINEBot.db;
 import com.code4unb.TemperatureLINEBot.UserData;
 import com.code4unb.TemperatureLINEBot.UserData.Grades;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
 import lombok.With;
+import lombok.experimental.Wither;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
+@Table("USER_DATA")
+@EqualsAndHashCode(of = {"ID"})
 public class UserDataEntity {
     @Column("id")
     @Id
-    @With
+    @Wither
     private final String ID;
 
     //Encrypt
-    @Column("line_id")
+    @Column("LINE_ID")
     private final String LineID;
 
-    @Column("grade")
+    @Column("GRADE")
     private final Grades Grade;
 
-    @Column("class")
+    @Column("CLASS")
     private final int Class_;
 
-    @Column("number")
+    @Column("NUMBER")
     private int Number;
 
     //Encrypt
-    @Column("first_name")
+    @Column("FIRST_NAME")
     private final String FirstName;
 
     //Encrypt
-    @Column("last_name")
+    @Column("LAST_NAME")
     private final String LastName;
 
     public UserDataEntity(String id,String lineID,Grades grade,int class_,int number,String firstName,String lastName){
