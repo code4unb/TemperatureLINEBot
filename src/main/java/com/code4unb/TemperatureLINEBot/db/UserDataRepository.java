0a74065b6f7349c80544317fd3df14fef2881074
tkhs <38522336+tkhs-dev@users.noreply.github.com>
2021-06-10 00:18:02 +0900

    feature: add new QUERY,find user by LINEID

--- src/main/java/com/code4unb/TemperatureLINEBot/db/UserDataRepository.java
+++ src/main/java/com/code4unb/TemperatureLINEBot/db/UserDataRepository.java
@@ -1,6 +1,12 @@
 package com.code4unb.TemperatureLINEBot.db;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserDataRepository extends CrudRepository<UserDataEntity,String> {
+    @Query("SELECT * FROM USER_DATA WHERE LINE_ID=:lineID")
+    Optional<UserDataEntity> findByLineID(@Param("lineID") String lineID);
 }
