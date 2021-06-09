72e1f3c95d8f9ff5d8ea2e9686a0039faeed513f
tokuhausu <38522336+tokuhausu@users.noreply.github.com>
2021-06-09 17:03:44 +0900

    fix: change field names to upper-case

--- src/main/java/com/code4unb/TemperatureLINEBot/db/UserDataEntity.java
+++ src/main/java/com/code4unb/TemperatureLINEBot/db/UserDataEntity.java
@@ -2,58 +2,56 @@ package com.code4unb.TemperatureLINEBot.db;
 
 import com.code4unb.TemperatureLINEBot.UserData;
 import com.code4unb.TemperatureLINEBot.UserData.Grades;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Setter;
-import lombok.With;
-import lombok.experimental.Wither;
+import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
-@Data
 @Table("USER_DATA")
-@EqualsAndHashCode(of = {"ID"})
+@Getter
+@EqualsAndHashCode(of = {"id"})
 public class UserDataEntity {
-    @Column("id")
+    @Column("ID")
     @Id
-    @Wither
-    private final String ID;
+    @With
+    private final String id;
 
     //Encrypt
     @Column("LINE_ID")
-    private final String LineID;
+    private final String lineId;
 
     @Column("GRADE")
-    private final Grades Grade;
+    private final Grades grade;
 
     @Column("CLASS")
-    private final int Class_;
+    private final int class_;
 
     @Column("NUMBER")
-    private int Number;
+    private final int number;
 
     //Encrypt
     @Column("FIRST_NAME")
-    private final String FirstName;
+    private final String firstName;
 
     //Encrypt
     @Column("LAST_NAME")
-    private final String LastName;
+    private final String lastName;
 
-    public UserDataEntity(String id,String lineID,Grades grade,int class_,int number,String firstName,String lastName){
-        ID = id;
-        LineID = lineID;
-        Grade = grade;
-        Class_ = class_;
-        Number = number;
-        FirstName = firstName;
-        LastName = lastName;
+    @PersistenceConstructor
+    public UserDataEntity(String id,String lineId,Grades grade,int class_,int number,String firstName,String lastName){
+        this.id = id;
+        this.lineId = lineId;
+        this.grade = grade;
+        this.class_ = class_;
+        this.number = number;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
     public UserDataEntity(String id,UserData data){
         this(id,data.getLineID(),data.getGrade(),data.getClass_(),data.getNumber(),data.getFirstName(),data.getLastName());
     }
     public UserData toUserData(){
-        return new UserData(getLineID(),getGrade(),getClass_(),getNumber(),getFirstName(),getLastName());
+        return new UserData(getLineId(),getGrade(),getClass_(),getNumber(),getFirstName(),getLastName());
     }
 }
