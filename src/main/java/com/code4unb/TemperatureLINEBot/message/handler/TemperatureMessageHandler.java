20c5c1b992674ca05bac2ffcae62119690ca8868
tkhs <38522336+tkhs-dev@users.noreply.github.com>
2021-06-19 00:25:20 +0900

    feature: under construction

--- src/main/java/com/code4unb/TemperatureLINEBot/message/handler/TemperatureMessageHandler.java
+++ src/main/java/com/code4unb/TemperatureLINEBot/message/handler/TemperatureMessageHandler.java
@@ -1,6 +1,8 @@
 package com.code4unb.TemperatureLINEBot.message.handler;
 
+import com.code4unb.TemperatureLINEBot.message.Flow;
 import com.code4unb.TemperatureLINEBot.message.FlowMessageHandler;
+import com.code4unb.TemperatureLINEBot.message.FlowResult;
 import com.code4unb.TemperatureLINEBot.model.MeasurementData;
 import com.code4unb.TemperatureLINEBot.model.ReceivedMessage;
 import com.linecorp.bot.model.message.Message;
@@ -8,8 +10,10 @@ import com.linecorp.bot.model.message.TextMessage;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class TemperatureMessageHandler extends FlowMessageHandler {
@@ -23,41 +27,69 @@ public class TemperatureMessageHandler extends FlowMessageHandler {
         if(super.shouldHandle(phrase)){
             return true;
         }else{
-            return checkFloat(phrase);
+            return parseFloat(phrase).isPresent();
         }
+    }
+
+    @Override
+    protected List<Flow> initFlows() {
+        return Arrays.asList(
+                new Flow() {
+                    @Override
+                    public Optional<List<Message>> postHandle() {
+                        return Optional.of(Collections.singletonList(TextMessage.builder().text("体温を入力してください。").build()));
+                    }
+
+                    @Override
+                    public FlowResult handle(ReceivedMessage message) {
+                        if(parseFloat(message.getKeyPhrase()).isPresent()){
+                            return new FlowResult(Optional.empty(),true);
+                        }else{
+                            return new FlowResult(Optional.of(Collections.singletonList(TextMessage.builder().text("不正な入力値 " + message.getKeyPhrase() + " です。体温は33℃から43℃の範囲内で入力してください。").build())),false);
+                        }
+                    }
+                },
+                new Flow(){
+
+                    @Override
+                    public Optional<List<Message>> postHandle() {
+                        return Optional.of(Collections.singletonList(TextMessage.builder().text("午前/午後(AM/PM)を入力してください。").build()));
+                    }
+
+                    @Override
+                    public FlowResult handle(ReceivedMessage message) {
+                        return new FlowResult(Optional.empty(),true);
+                    }
+                }
+        );
     }
 
     @Override
     protected List<Message> handleActivateMessage(ReceivedMessage message) {
-        if(checkFloat(message.getKeyPhrase())){
+        Optional<Float> parsed = parseFloat(message.getKeyPhrase());
+        if (parsed.isPresent()) {
             close();
-            float input = Float.parseFloat(message.getKeyPhrase());
-            if(33f <= input && input < 43f){
-                MeasurementData data =  MeasurementData.builder()
-                        .temperature(String.format("%.1f",input))
+            float input = parsed.get();
+            if (33f <= input && input < 43f) {
+                MeasurementData data = MeasurementData.builder()
+                        .temperature(String.format("%.1f", input))
                         .date(LocalDate.now())
                         .convention(MeasurementData.TimeConvention.Now())
                         .build();
                 return Collections.singletonList(TextMessage.builder().text(data.toString()).build());
-            }else{
-                return Collections.singletonList(TextMessage.builder().text("不正な入力値 "+input+" です。33℃から43℃の範囲内で入力してください。").build());
+            } else {
+                return Collections.singletonList(TextMessage.builder().text("不正な入力値 " + input + " です。体温は33℃から43℃の範囲内で入力してください。").build());
             }
-        }else{
-            return Collections.singletonList(TextMessage.builder().text("体温を入力してください。").build());
+        } else {
+            return null;
         }
     }
 
-    @Override
-    protected List<Message> handleSessionMessage(ReceivedMessage message) {
-        return null;
-    }
-
-    private boolean checkFloat(String text){
+    private Optional<Float> parseFloat(String text){
         try{
-            Float.parseFloat(text);
-            return true;
+            return Optional.of(Float.parseFloat(text));
         }catch(Exception e){
-            return false;
+            return Optional.empty();
         }
     }
 }
