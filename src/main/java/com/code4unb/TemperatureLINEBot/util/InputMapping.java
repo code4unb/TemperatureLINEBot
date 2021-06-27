1e312cda2f3ed1eef2479de0a044d3d24d23619f
tkhs <38522336+tkhs-dev@users.noreply.github.com>
2021-06-27 22:41:01 +0900

    fix: resource importation

--- src/main/java/com/code4unb/TemperatureLINEBot/util/InputMapping.java
+++ src/main/java/com/code4unb/TemperatureLINEBot/util/InputMapping.java
@@ -8,9 +8,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.security.CodeSource;
 import java.util.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 @Data
 @AllArgsConstructor
@@ -26,20 +33,66 @@ public class InputMapping {
 
     static Map<UserData.ClassRoom,InputMapping> mappings;
 
-    static{
+    static {
         mappings = new HashMap<>();
 
+        URI uri = null;
+
+         try {
+            uri = InputMapping.class.getClassLoader().getResource(DIR).toURI();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e){
+            e.printStackTrace();
+        }
+
         Pattern pattern = Pattern.compile("mapping_([0-9])_([0-9]+).json");
-        InputMapping.class.getClassLoader().resources(DIR)
-                .flatMap(x-> Arrays.stream(new File(x.getFile()).listFiles()).map(y->y.getName()))
-                .map(x->pattern.matcher(x))
-                .filter(x->x.matches())
-                .forEach(x->{
-                    UserData.Grades grade = UserData.Grades.values()[ Integer.parseInt(x.group(1))-1];
-                    int _class = Integer.parseInt(x.group(2));
-                    UserData.ClassRoom classRoom = new UserData.ClassRoom(grade,_class);
-                    mappings.put(classRoom,load(classRoom));
-                });
+
+        if(uri.getScheme().contains("jar")){
+            CodeSource src = InputMapping.class.getProtectionDomain().getCodeSource();
+            if (src != null) {
+                URL jar = src.getLocation();
+                ZipInputStream zip = null;
+                try {
+                    zip = new ZipInputStream(jar.openStream());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                while(true) {
+                    ZipEntry e = null;
+                    try {
+                        e = zip.getNextEntry();
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                    if (e == null)
+                        break;
+                    String name = e.getName();
+                    if (name.startsWith("BOOT-INF/classes/"+DIR)) {
+                        String file = name.replace("BOOT-INF/classes/"+DIR+"/","");
+                        Matcher match = pattern.matcher(file);
+                        if(match.matches()){
+                            UserData.Grades grade = UserData.Grades.values()[ Integer.parseInt(match.group(1))-1];
+                            int _class = Integer.parseInt(match.group(2));
+                            UserData.ClassRoom classRoom = new UserData.ClassRoom(grade,_class);
+                            mappings.put(classRoom,load(classRoom));
+                        }
+                    }
+                }
+            }
+        }
+        else{
+            InputMapping.class.getClassLoader().resources(DIR)
+                    .flatMap(x-> Arrays.stream(new File(x.getFile()).listFiles()).map(y->y.getName()))
+                    .map(x->pattern.matcher(x))
+                    .filter(x->x.matches())
+                    .forEach(x->{
+                        UserData.Grades grade = UserData.Grades.values()[ Integer.parseInt(x.group(1))-1];
+                        int _class = Integer.parseInt(x.group(2));
+                        UserData.ClassRoom classRoom = new UserData.ClassRoom(grade,_class);
+                        mappings.put(classRoom,load(classRoom));
+                    });
+        }
     }
 
     public static Optional<InputMapping> getInstance(UserData.ClassRoom classRoom){
