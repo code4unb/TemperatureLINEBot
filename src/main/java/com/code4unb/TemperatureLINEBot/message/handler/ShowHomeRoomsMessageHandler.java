828414d199791d8dd36da6ee7ed74093d5990ea0
tkhs <38522336+tkhs-dev@users.noreply.github.com>
2021-06-27 23:56:20 +0900

    fix: add @Component

--- src/main/java/com/code4unb/TemperatureLINEBot/message/handler/ShowHomeRoomsMessageHandler.java
+++ src/main/java/com/code4unb/TemperatureLINEBot/message/handler/ShowHomeRoomsMessageHandler.java
@@ -6,12 +6,14 @@ import com.code4unb.TemperatureLINEBot.model.UserData;
 import com.code4unb.TemperatureLINEBot.util.InputMapping;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
+import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class ShowHomeRoomsMessageHandler extends SingleMessageHandler {
     public ShowHomeRoomsMessageHandler(){
         super("対応ホームルーム一覧","クラス","対応","対応クラス","ホームルーム");
