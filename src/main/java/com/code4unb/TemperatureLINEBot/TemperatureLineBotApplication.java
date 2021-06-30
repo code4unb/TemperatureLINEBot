7261f554c602391b415cf332b20eecfe8d43002d
tkhs <38522336+tkhs-dev@users.noreply.github.com>
2021-06-30 22:14:18 +0900

    refactor: remove unused event handler

--- src/main/java/com/code4unb/TemperatureLINEBot/TemperatureLineBotApplication.java
+++ src/main/java/com/code4unb/TemperatureLINEBot/TemperatureLineBotApplication.java
@@ -1,11 +1,6 @@
 package com.code4unb.TemperatureLINEBot;
 
 import com.linecorp.bot.client.LineMessagingClient;
-import com.linecorp.bot.model.event.Event;
-import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,17 +19,6 @@ public class TemperatureLineBotApplication {
 
 	private static void initialize(){
 		//client = LineMessagingClient.builder(token).build();
-	}
-
-	@EventMapping
-	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event){
-		System.out.println("event:"+event);
-		return new TextMessage(event.getMessage().getText());
-	}
-
-	@EventMapping
-	public void handleDefaultMessageEvent(Event event) {
-		System.out.println("event: " + event);
 	}
 
 }
