cba0fc8efa8f48002632ef33ce925bad2b1ea4e2
tkhs <38522336+tkhs-dev@users.noreply.github.com>
2021-07-03 11:27:33 +0900

    feature: rewrite FlowResult builder

--- src/main/java/com/code4unb/TemperatureLINEBot/message/FlowResult.java
+++ src/main/java/com/code4unb/TemperatureLINEBot/message/FlowResult.java
@@ -4,16 +4,29 @@ import com.linecorp.bot.model.message.Message;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Singular;
 
 import java.util.List;
 import java.util.Optional;
 
 @Getter
-@Builder
+@Builder()
 @AllArgsConstructor
 public class FlowResult {
-    @Builder.Default
-    private Optional<List<Message>> result = Optional.empty();
+    public static final FlowResult EMPTY_SUCCEED = FlowResult.builder().succeed(true).build();
+
+    public  static final FlowResult EMPTY_FAILED = FlowResult.builder().succeed(false).build();
+
+    @Singular("singletonResult")
+    private List<Message> result;
 
     private boolean succeed;
+
+    public Optional<List<Message>> getResult(){
+        if(result == null || result.isEmpty()){
+            return Optional.empty();
+        }else{
+            return Optional.of(result);
+        }
+    }
 }
